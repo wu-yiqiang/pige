04c4433f8d3bfd232fc3b234ffaf2d39f9aad1ff
wu-yiqiang <wu_yiqiang@outlook.com>
2024-09-06 01:39:46 +0800

    清除冲突

--- frontend/src/Layout/Pannel.vue
+++ frontend/src/Layout/Pannel.vue
@@ -11,15 +11,6 @@ import {reactive, watch, computed, ref, onMounted} from 'vue'
 import Item from '@/chat/item.vue'
 import {User, users} from '../utils/const'
 const lists: Array<User> = reactive(users)
-<<<<<<< Updated upstream
-const getDeviceLists = async () => {
-  const data = await GetDevices()
-  console.log("a萨达身上das", data)
-}
-onMounted(
-)
-=======
->>>>>>> Stashed changes
 </script>
 <style lang="scss" scoped>
 .List{

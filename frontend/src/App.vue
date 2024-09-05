6e36d940c5e7745396744ceeaeb1d1b62dfc6742
wu-yiqiang <wu_yiqiang@outlook.com>
2024-09-06 01:39:06 +0800

    清除冲突·

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -17,12 +17,8 @@ import Pannel from '@/Layout/Pannel.vue'
 import Window from '@/Chat/window.vue'
 import { storeRouter } from './store/index';
 import { computed, onMounted } from 'vue';
-<<<<<<< Updated upstream
-=======
 import { GetDevices } from '../wailsjs/go/main/App'
->>>>>>> Stashed changes
 const store = storeRouter()
-import { GetDevices } from '../wailsjs/go/main/App'
 const getDeviceLists = async () => {
   const data = await GetDevices()
   console.log("a萨达身上das", data)
@@ -30,21 +26,11 @@ const getDeviceLists = async () => {
 const isFullWidth = computed(() => {
 return store.fullWidth
 })
-<<<<<<< Updated upstream
-onMounted(() => {
-  getDeviceLists()
-})
-=======
-const getDeviceLists = async () => {
-  const data = await GetDevices()
-  console.log("asdas", data)
-}
 onMounted(
     () => {
       getDeviceLists()
     }
 )
->>>>>>> Stashed changes
 </script>
 
 <style lang="scss">
